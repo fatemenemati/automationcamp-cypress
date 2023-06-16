3bde992bb1ec61573b013fcdd5f9a0ca00b92adc
fatemeh <fatemenemati1998@gmail.com>
2023-06-16 17:48:00 +0330

    Automationcamp-cypress code to learning.

---
+++ cypress/support/e2e.js
@@ -0,0 +1,10 @@
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+import './commands2'
+import "cypress-real-events/support";
+import "cypress-iframe"
+require('cy-verify-downloads').addCustomCommand();
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
